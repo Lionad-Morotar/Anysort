d8cd5f0bf32489b301ce0dcc0b4c094ac2445d0e
仿生狮子 <tangnad@qq.com>
2022-03-09 14:40:33 +0800

    chore: install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'array-callback-return': 0,
+    'no-return-assign': 0
+  }
+}
