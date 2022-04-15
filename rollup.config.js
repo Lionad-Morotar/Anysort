388dd0114a5bca90f37e7776daeb7a3d6391ba01
仿生狮子 <tangnad@qq.com>
2022-04-15 17:42:26 +0800

    chore: update depends

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,7 @@
+import common from '@rollup/plugin-commonjs'
+import node from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
 const path = require('path')
 
 const input = path.join(__dirname, './src/index.ts')
@@ -8,16 +12,15 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   input,
   plugins: [
-    require('rollup-plugin-commonjs')(),
-    require('rollup-plugin-node-resolve')(),
-    require('rollup-plugin-typescript')(),
-    // !isDev && require('rollup-plugin-uglify')()
+    common(),
+    node(),
+    typescript()
   ],
   external: [/node_modules/],
   output: {
     name: 'module',
     file: ouput + (isDev ? '.js' : '.min.js'),
     format: 'umd',
-    sourcemap: true,
+    sourcemap: true
   }
 }
